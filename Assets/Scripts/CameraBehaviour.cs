e21cf41568fd4d1036d4f26167b5e2a34d52acd0
Say2Slay <56179065+Say2Slay@users.noreply.github.com>
2020-01-31 23:24:25 +0100

    Prefabs

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    Transform Target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 spot = Target.position + offset;
+        Vector3 smoothMove = Vector3.Lerp
+    }
+}
