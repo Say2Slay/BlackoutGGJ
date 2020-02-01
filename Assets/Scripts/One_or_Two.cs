7f0e8415fde7349005bbb569dc30ca5356d2d8cf
estebanluginbuhl <estebanluginbuhl@gmail.com>
2020-02-02 00:39:43 +0100

    script choose team

---
+++ Assets/Scripts/One_or_Two.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class One_or_Two : MonoBehaviour
+{
+    public void ChooseTeamElonMusk()
+    {
+        //prendre white hat
+    }
+
+    public void ChooseTeamWakanda()
+    {
+        //prendre black hat
+    }
+}
