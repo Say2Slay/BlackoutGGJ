8eb6943cea605a33f03ed4b8f1c18acbc7f89a3b
Say2Slay <56179065+Say2Slay@users.noreply.github.com>
2020-02-01 02:19:22 +0100

    Noclip test

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -8,19 +8,42 @@ public class CharacterBehaviour : MonoBehaviour
     public float speedBuffer = 1;
     private float x, z;
 
+    public float delay = 10f;
+    float cooldown;
+
+    public int Team;
+
+    void Start()
+    {
+        cooldown = delay;
+    }
+
     void Update()
     {
         x = Input.GetAxis("Horizontal") * moveSpeed * speedBuffer * Time.deltaTime;
         z = Input.GetAxis("Vertical") * moveSpeed * speedBuffer * Time.deltaTime;
 
         transform.Translate(x, 0, z);
+
+        if (Input.GetButtonDown("Ulti"))
+        {
+            if (Team == 1 && cooldown <= 0)
+                StartCoroutine(Noclip());
+        }
+
+        cooldown -= Time.deltaTime;
     }
 
-    void OnTriggerEnter(Collider other)
+    IEnumerator Noclip()
     {
-        if (other.gameObject.tag.Equals("Interactable"))
-        {
-            other.GetComponent<Interaction>().isNearPlayer = true;
-        }
+        gameObject.GetComponent<Collider>().enabled = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+
+        yield return new WaitForSeconds(5f);
+
+        gameObject.GetComponent<Collider>().enabled = true;
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+        cooldown = delay;
     }
 }
